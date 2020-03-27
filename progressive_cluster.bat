5bb8b8dbf7232fe8ae7267c23b1daab8cead11f5
sejunssi <skim3393@gmail.com>
2020-03-27 08:52:04 -0500

    Write the csv reuslt

---
+++ progressive_cluster.bat
@@ -0,0 +1,16 @@
+
+python main_SBMs_node_classification.py --dataset SBM_CLUSTER --gpu_id 1 --seed 41 --config configs/SBMs_node_clustering_GCN_CLUSTER.json
+python main_SBMs_node_classification.py --dataset SBM_CLUSTER --gpu_id 1 --seed 95 --config configs/SBMs_node_clustering_GCN_CLUSTER.json
+python main_SBMs_node_classification.py --dataset SBM_CLUSTER --gpu_id 1 --seed 12 --config configs/SBMs_node_clustering_GCN_CLUSTER.json
+
+python main_SBMs_node_classification.py --dataset SBM_CLUSTER --gpu_id 1 --seed 35 --config configs/SBMs_node_clustering_GCN_CLUSTER.json
+python main_SBMs_node_classification.py --dataset SBM_CLUSTER --gpu_id 1 --seed 41 --config configs/SBMs_node_clustering_GraphSage_CLUSTER.json
+python main_SBMs_node_classification.py --dataset SBM_CLUSTER --gpu_id 1 --seed 95 --config configs/SBMs_node_clustering_GraphSage_CLUSTER.json
+
+python main_SBMs_node_classification.py --dataset SBM_CLUSTER --gpu_id 1 --seed 12 --config configs/SBMs_node_clustering_GraphSage_CLUSTER.json
+python main_SBMs_node_classification.py --dataset SBM_CLUSTER --gpu_id 1 --seed 35 --config configs/SBMs_node_clustering_GraphSage_CLUSTER.json
+python main_SBMs_node_classification.py --dataset SBM_CLUSTER --gpu_id 1 --seed 41 --config configs/SBMs_node_clustering_GAT_CLUSTER.json
+
+python main_SBMs_node_classification.py --dataset SBM_CLUSTER --gpu_id 1 --seed 95 --config configs/SBMs_node_clustering_GAT_CLUSTER.json
+python main_SBMs_node_classification.py --dataset SBM_CLUSTER --gpu_id 1 --seed 12 --config configs/SBMs_node_clustering_GAT_CLUSTER.json
+python main_SBMs_node_classification.py --dataset SBM_CLUSTER --gpu_id 1 --seed 35 --config configs/SBMs_node_clustering_GAT_CLUSTER.json
---
+++ progressive_pattern.bat
@@ -0,0 +1,16 @@
+
+python main_SBMs_node_classification.py --dataset SBM_PATTERN --gpu_id 2 --seed 41 --config configs/SBMs_node_clustering_GCN_PATTERN.json
+python main_SBMs_node_classification.py --dataset SBM_PATTERN --gpu_id 2 --seed 95 --config configs/SBMs_node_clustering_GCN_PATTERN.json
+python main_SBMs_node_classification.py --dataset SBM_PATTERN --gpu_id 2 --seed 12 --config configs/SBMs_node_clustering_GCN_PATTERN.json
+
+python main_SBMs_node_classification.py --dataset SBM_PATTERN --gpu_id 2 --seed 35 --config configs/SBMs_node_clustering_GCN_PATTERN.json
+python main_SBMs_node_classification.py --dataset SBM_PATTERN --gpu_id 2 --seed 41 --config configs/SBMs_node_clustering_GraphSage_PATTERN.json
+python main_SBMs_node_classification.py --dataset SBM_PATTERN --gpu_id 2 --seed 95 --config configs/SBMs_node_clustering_GraphSage_PATTERN.json
+
+python main_SBMs_node_classification.py --dataset SBM_PATTERN --gpu_id 2 --seed 2 --config configs/SBMs_node_clustering_GraphSage_PATTERN.json
+python main_SBMs_node_classification.py --dataset SBM_PATTERN --gpu_id 2 --seed 35 --config configs/SBMs_node_clustering_GraphSage_PATTERN.json
+python main_SBMs_node_classification.py --dataset SBM_PATTERN --gpu_id 2 --seed 41 --config configs/SBMs_node_clustering_GAT_PATTERN.json
+
+python main_SBMs_node_classification.py --dataset SBM_PATTERN --gpu_id 2 --seed 95 --config configs/SBMs_node_clustering_GAT_PATTERN.json
+python main_SBMs_node_classification.py --dataset SBM_PATTERN --gpu_id 2 --seed 12 --config configs/SBMs_node_clustering_GAT_PATTERN.json
+python main_SBMs_node_classification.py --dataset SBM_PATTERN --gpu_id 2 --seed 35 --config configs/SBMs_node_clustering_GAT_PATTERN.json
